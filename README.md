fb314ed5d6e87c6b9248cea4812f82a9b3090a5f
Paul Kehrer <pkehrer@cimpress.com>
2024-12-10 15:36:36 -0500

    a second off 6

--- README.md
+++ README.md
@@ -1,35 +1,35 @@
 # Ho ho ho...
 ```
 Day 01:
-	Part 1: 2378066  execution time: 56.8ms
-	Part 2: 18934359  execution time: 28.6ms
+	Part 1: 2378066  execution time: 57.5ms
+	Part 2: 18934359  execution time: 30.1ms
 Day 02:
-	Part 1: 639  execution time: 1.1ms
-	Part 2: 674  execution time: 1.6ms
+	Part 1: 639  execution time: 1.5ms
+	Part 2: 674  execution time: 1.7ms
 Day 03:
-	Part 1: 191183308  execution time: 0.7ms
-	Part 2: 92082041  execution time: 0.4ms
+	Part 1: 191183308  execution time: 1.2ms
+	Part 2: 92082041  execution time: 0.5ms
 Day 04:
-	Part 1: 2530  execution time: 27.4ms
-	Part 2: 1921  execution time: 7.2ms
+	Part 1: 2530  execution time: 27.8ms
+	Part 2: 1921  execution time: 7.7ms
 Day 05:
-	Part 1: 4996  execution time: 6.0ms
-	Part 2: 6311  execution time: 298.6ms
+	Part 1: 4996  execution time: 6.8ms
+	Part 2: 6311  execution time: 304.2ms
 Day 06:
-	Part 1: 5153  execution time: 1.7ms
-	Part 2: 1711  execution time: 4982.7ms
+	Part 1: 5153  execution time: 1.9ms
+	Part 2: 1711  execution time: 4562.5ms
 Day 07:
-	Part 1: 66343330034722  execution time: 38.7ms
-	Part 2: 637696070419031  execution time: 4362.6ms
+	Part 1: 66343330034722  execution time: 40.7ms
+	Part 2: 637696070419031  execution time: 4631.3ms
 Day 08:
-	Part 1: 273  execution time: 0.9ms
-	Part 2: 1017  execution time: 18.1ms
+	Part 1: 273  execution time: 1.1ms
+	Part 2: 1017  execution time: 18.6ms
 Day 09:
-	Part 1: 6448989155953  execution time: 11.7ms
-	Part 2: 6476642796832  execution time: 32.0ms
+	Part 1: 6448989155953  execution time: 12.3ms
+	Part 2: 6476642796832  execution time: 32.3ms
 Day 10:
-	Part 1: 646  execution time: 7.3ms
-	Part 2: 1494  execution time: 7.2ms
+	Part 1: 646  execution time: 7.6ms
+	Part 2: 1494  execution time: 7.3ms
 
-  Total execution time: 9.91s
+  Total execution time: 9.78s
 ```
--- day06.py
+++ day06.py
@@ -1,7 +1,8 @@
 """
 https://adventofcode.com/2024/day/6
 """
-from util import get_puzzle_input_lines, run_file
+from util import get_puzzle_input_lines, run_file, start_timer, get_elapsed_ms
+import copy
 
 
 def parse_input():
@@ -13,32 +14,30 @@ def find_guard(map):
         if '^' in row:
             return row.index('^'), y
 
-
+direction_changes = {
+    1: 2,
+    2: 3,
+    3: 4,
+    4: 1
+}
 def turn90(direction):
-    if direction == 'up':
-        return 'right'
-    if direction == 'right':
-        return 'down'
-    if direction == 'down':
-        return 'left'
-    if direction == 'left':
-        return 'up'
+   global direction_changes
+   return direction_changes[direction]
 
-def next_step(x, y, direction):
-    if direction == 'up':
-        return x, y-1
-    if direction == 'right':
-        return x+1, y
-    if direction == 'down':
-        return x, y+1
-    if direction == 'left':
-        return x-1, y
-    raise RuntimeError("this shouldn't happen")
+direction_coordinate_changes = {
+        1: lambda x,y: (x, y-1),
+        2: lambda x,y: (x+1, y),
+        3: lambda x,y: (x, y+1),
+        4: lambda x,y: (x-1, y)
+    }
+def next_step(cur_x, cur_y, direction):
+    global direction_coordinate_changes
+    return direction_coordinate_changes[direction](cur_x, cur_y)
 
 def walk_map(the_map):
     x, y = find_guard(the_map)
     the_map[y][x] = 'X'
-    direction = 'up'
+    direction = 1
     while True:
         nextx, nexty = next_step(x, y, direction)
         if nextx < 0 or nextx >= len(the_map[0]) or nexty < 0 or nexty >= len(the_map):
@@ -62,32 +61,32 @@ def part1():
     return score
 
 
-def map_has_loop(the_map):
-    x, y = find_guard(the_map)
-    the_map[y][x] = 'X'
-    direction = 'up'
+def map_has_loop(guard_x, guard_y, the_map):
+    the_map[guard_y][guard_x] = 'X'
+    direction = 1
 
     while True:
-        nextx, nexty = next_step(x,y, direction)
+        next_x, next_y = next_step(guard_x,guard_y, direction)
 
-        if nextx < 0 or nextx >= len(the_map[0]) or nexty < 0 or nexty >= len(the_map):
+        if next_x < 0 or next_x >= len(the_map[0]) or next_y < 0 or next_y >= len(the_map):
             return False
 
-        next_spot = the_map[nexty][nextx]
+        next_spot = the_map[next_y][next_x]
 
         if next_spot == '#':
             direction = turn90(direction)
         else:
-            x = nextx
-            y = nexty
+            guard_x = next_x
+            guard_y = next_y
             if next_spot == direction:
                 return True
             else:
-                the_map[nexty][nextx] = direction
+                the_map[next_y][next_x] = direction
 
 
 def part2():
-    a_map = parse_input()
+    source_map = parse_input()
+    a_map = [[c for c in l] for l in source_map]
     guard_x, guard_y = find_guard(a_map)
     walk_map(a_map)
     possible_blocker_positions = []
@@ -99,14 +98,15 @@ def part2():
 
     good_spots = 0
     for blocker_x, blocker_y in possible_blocker_positions:
-        the_map = parse_input()
+        the_map = [[c for c in l] for l in source_map]
         the_map[blocker_y][blocker_x] = '#'
-        if map_has_loop(the_map):
+        if map_has_loop(guard_x, guard_y, the_map):
             good_spots += 1
 
     return good_spots
 
 run_file()
-# Day 6:
-# 	Part 1: 5153  execution time: 2.5ms
-# 	Part 2: 1711  execution time: 5250.3ms
+# Day 06:
+# 	Part 1: 5153  execution time: 2.3ms
+# 	Part 2: 1711  execution time: 4526.1ms
+
