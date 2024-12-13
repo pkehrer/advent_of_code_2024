58df412d13b4dad2baab5f45337299ed04e37946
Paul Kehrer <pkehrer@cimpress.com>
2024-12-13 12:38:03 -0500

    meh

--- README.md
+++ README.md
@@ -1,44 +1,45 @@
 # Ho ho ho...
+
 ```
 Day 1 🍪
-	Part 1: 2378066           execution time: 56.1ms
-	Part 2: 18934359          execution time: 30.2ms
+	Part 1: 2378066           execution time: 59.5ms
+	Part 2: 18934359          execution time: 29.0ms
 Day 2 ⛄
 	Part 1: 639               execution time: 1.9ms
-	Part 2: 674               execution time: 1.7ms
+	Part 2: 674               execution time: 1.6ms
 Day 3 🧣
 	Part 1: 191183308         execution time: 1.3ms
-	Part 2: 92082041          execution time: 0.6ms
+	Part 2: 92082041          execution time: 0.5ms
 Day 4 🦌
-	Part 1: 2530              execution time: 28.1ms
-	Part 2: 1921              execution time: 7.9ms
+	Part 1: 2530              execution time: 27.9ms
+	Part 2: 1921              execution time: 7.7ms
 Day 5 🔔
-	Part 1: 4996              execution time: 6.5ms
-	Part 2: 6311              execution time: 313.9ms
+	Part 1: 4996              execution time: 6.1ms
+	Part 2: 6311              execution time: 295.0ms
 Day 6 🫎
 	Part 1: 5153              execution time: 2.3ms
-	Part 2: 1711              execution time: 4188.3ms
+	Part 2: 1711              execution time: 3778.3ms
 Day 7 👼
-	Part 1: 66343330034722    execution time: 40.2ms
-	Part 2: 637696070419031   execution time: 4785.8ms
+	Part 1: 66343330034722    execution time: 39.1ms
+	Part 2: 637696070419031   execution time: 1575.6ms
 Day 8 🐻
-	Part 1: 273               execution time: 1.2ms
-	Part 2: 1017              execution time: 19.5ms
+	Part 1: 273               execution time: 1.0ms
+	Part 2: 1017              execution time: 18.1ms
 Day 9 🎄
-	Part 1: 6448989155953     execution time: 12.4ms
-	Part 2: 6476642796832     execution time: 32.9ms
+	Part 1: 6448989155953     execution time: 12.0ms
+	Part 2: 6476642796832     execution time: 30.9ms
 Day 10 🌟
-	Part 1: 646               execution time: 8.0ms
-	Part 2: 1494              execution time: 7.4ms
+	Part 1: 646               execution time: 7.4ms
+	Part 2: 1494              execution time: 6.8ms
 Day 11 🤶
-	Part 1: 199946            execution time: 1.5ms
-	Part 2: 237994815702032   execution time: 53.5ms
+	Part 1: 199946            execution time: 1.4ms
+	Part 2: 237994815702032   execution time: 49.0ms
 Day 12 🎅
-	Part 1: 1550156           execution time: 36.6ms
-	Part 2: 946084            execution time: 45.5ms
+	Part 1: 1550156           execution time: 33.9ms
+	Part 2: 946084            execution time: 40.7ms
 Day 13 🎁
-	Part 1: 28059             execution time: 1.2ms
-	Part 2: 102255878088512   execution time: 0.8ms
+	Part 1: 28059             execution time: 1.0ms
+	Part 2: 102255878088512   execution time: 0.6ms
 
-Total execution time: 9.69s
+Total execution time: 6.04s
 ```
--- day07.py
+++ day07.py
@@ -14,56 +14,56 @@ def parse_input():
     return records
 
 
-def get_possible_results(current_result, remaining_numbers):
-    if len(remaining_numbers) == 1:
-        return [current_result + remaining_numbers[0], current_result * remaining_numbers[0]]
-
-    plus_results = get_possible_results(current_result + remaining_numbers[0], remaining_numbers[1:])
-    mult_results = get_possible_results(current_result * remaining_numbers[0], remaining_numbers[1:])
-
-    return plus_results + mult_results
-
-def get_possible_results_with_pipe(current_result, numbers, index):
+def get_result(current_result: int, desired_result: int, numbers: list[int], index=0, include_pipe=False):
     if index == len(numbers):
-        return [current_result]
+        return current_result if current_result == desired_result else 0
 
-    plus_results = get_possible_results_with_pipe(
-        current_result + numbers[index],
-        numbers,
-        index+1)
-    mult_results = get_possible_results_with_pipe(
-        current_result * numbers[index],
-        numbers,
-        index+1)
-    pipe_results = get_possible_results_with_pipe(
-        int(str(current_result) + str(numbers[index])),
-        numbers,
-        index+1)
+    plus_result = get_result(current_result + numbers[index], desired_result, numbers, index + 1, include_pipe)
+    if plus_result > 0:
+        return plus_result
 
-    return plus_results + mult_results + pipe_results
+    mult_result = get_result(current_result * numbers[index], desired_result, numbers, index + 1, include_pipe)
+    if mult_result > 0:
+        return mult_result
+
+    if include_pipe:
+        result_of_pipe_operator = int(str(current_result) + str(numbers[index]))
+        pipe_results = get_result(result_of_pipe_operator, desired_result, numbers, index + 1, include_pipe)
+        if pipe_results > 0:
+            return pipe_results
+
+    return 0
+
 
 def part1():
     records = parse_input()
     score = 0
 
     for record in records:
-        possible_results = get_possible_results(record[1][0], record[1][1:])
-        if record[0] in possible_results:
-            score += record[0]
+        record_score = get_result(current_result=record[1][0],
+                                  desired_result=record[0],
+                                  numbers=record[1][1:])
+
+        score += record_score
 
     return score
+
 
 def part2():
     records = parse_input()
     score = 0
 
     for record in records:
-        possible_results = get_possible_results_with_pipe(record[1][0], record[1], 1)
-        if record[0] in possible_results:
-            score += record[0]
+        record_score = get_result(current_result=record[1][0],
+                                  desired_result=record[0],
+                                  numbers=record[1][1:],
+                                  index=0,
+                                  include_pipe=True)
+
+        score += record_score
 
     return score
 
-# Day 7:
-# 	Part 1: 66343330034722  execution time: 44.2ms
-# 	Part 2: 637696070419031  execution time: 4635.0ms
+# Day 7 👼
+# 	Part 1: 66343330034722    execution time: 44.9ms
+# 	Part 2: 637696070419031   execution time: 1602.3ms
