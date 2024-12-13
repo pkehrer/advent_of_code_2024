2633db7de7cfab2e01f6c3726693aacdb399747b
Paul Kehrer <pkehrer@cimpress.com>
2024-12-13 11:58:27 -0500

    readme

--- runner.py
+++ runner.py
@@ -1,13 +1,15 @@
 import util
 from text_util import christmas_text, red_text, green_text
 
+christmas_characters = '🍪⛄🧣🦌🔔🫎👼🐻🎄🌟🤶🎅🎁'
 
 def run_file(filename):
     module_name = filename.replace('.py', '')
     day = int(module_name.split('day')[1])
     module = __import__(module_name)
 
-    print(red_text('Day ') + green_text(f'{day} 🎄'))
+    icon = christmas_characters[(int(day)-1) % len(christmas_characters)]
+    print(red_text('Day ') + green_text(f'{day} ') + icon)
 
     for num, part in [(1, getattr(module, 'part1', None)), (2,getattr(module, 'part2', None))]:
         if part is None:
